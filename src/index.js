6d0d368b8867b0c3dd9a5dafaf6b7f76d5354e9e
Joe Sandmeyer <github.com@unconscious.com>
2023-08-04 19:05:02 -0400

    Tried Gerson's code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { BasePlugin, BaseComponent } from 'vatom-spaces-plugins'
+//import { BasePlugin, BaseComponent } from 'vatom-spaces-plugins'
 
 
+/***
 export default class StatusItemTextPlugin extends BasePlugin {
 
-	/** Plugin info */
+	// Plugin info 
 	static id = "status-item-text"
 	static name = "Status Item Text Plugin"
 	static description = "A Vatom Spaces plugin to test Status Item Text."
@@ -80,11 +81,85 @@ export default class StatusItemTextPlugin extends BasePlugin {
 
 
 
-/***
+ ***
 		this.myStatusItemId = await this.objects.createStatusItem({
 			userID: this.myUserId,
 			size: 1.0
 		},{
 			type: 'text', text: 'Hello', textBold: true
 		});
+		
+		
+		
+		
+module.exports = class test extends BasePlugin {
+		
  ***/
+ 
+// NOTE:
+// For manual upload of plugin use the following instead of
+// 'import  and 'export default' statements.
+// module.exports = class ...
+//
+// For sideloading from http://localhost:9000/plugin.js via yarn start use the following
+// import { BasePlugin, BaseComponent } from 'vatom-spaces-plugins'
+// export default class ...
+
+export default class StatusItemTextPlugin extends BasePlugin {
+
+	// Plugin info
+	static get id() {
+		return "Test Plugin";
+	}
+	static get name() {
+		return "Test Plugin";
+	}
+	static get description() {
+		return "Plugin to test some functionalities.";
+	}
+
+
+	async onLoad() {
+		// Globl configuration
+		this.menus.register({
+			id: 'EYFoundry.test-config',
+			section: 'plugin-settings',
+			panel: {
+					fields: [
+							{ type: 'text', id: 'objectID', name: 'Object ID', help: 'Set the object ID.' },
+							{ type: 'text', id: 'imageURL', name: 'Image URL', help: 'Set the image URL.' }                                                            
+					]
+			}
+		});
+
+		const menuId = await this.menus.register({
+			id: 'EYFoundry.get-help',
+			title: 'Test',
+			text: 'Test',
+			adminOnly: false,
+			currentUser: true,
+			inAccordion: true,
+			section: 'controls',
+			icon: '',
+			action: e => this.testPlugin(e)
+		});
+
+		const id = await this.objects.createStatusItem({
+			userID: 'vatominc:brapm9i',
+			size: 0.2, width: 10, height: 10
+		},{
+			type: 'text', text: 'Hello', textBold: true
+		});
+	}
+
+
+	async testPlugin() {
+		const objectID = this.getField('objectID') || '';
+		const imageURL = this.getField('imageURL') || '';
+		await this.objects.update(objectID, {url: imageURL}, false);                
+	}
+
+	onUnload() {}
+}
+
+ 
